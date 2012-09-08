9a5a13ef2f7254159ecaceea33a1da7ba7463767
Raphael Stäbler <blazer@fantasymail.de>
2012-09-08 17:24:07 +0200

    MOD: some first gravitational tests (rendering is likely to be wrong)

--- public/js/universe.js
+++ public/js/universe.js
@@ -1,9 +1,20 @@
 
 var Universe = function(elementId)
 {
-    this.particleSize = 16;
+    this.particleSize  = 32;
+    this.gravityRadius = 4;
+    this.gravityFactor = 1;
+
+    this.particles = [];
 
     var $universe = $('#universe');
+
+    this.size = {
+        width: Math.floor($universe.innerWidth() / this.particleSize),
+        height: Math.floor($universe.innerHeight() / this.particleSize)
+    };
+
+    log('Universe initialized with size '+this.size.width+'x'+this.size.height);
 
     this.stage = new Kinetic.Stage({
         container: elementId,
@@ -11,7 +22,10 @@ var Universe = function(elementId)
         height: $universe.innerHeight()
     });
 
-    log('Universe initialized with size '+this.stage.getWidth()+'x'+this.stage.getHeight());
+    this.layer = new Kinetic.Layer();
+    this.stage.add(this.layer);
+
+    log('Stage initialized with size '+this.stage.getWidth()+'x'+this.stage.getHeight());
 };
 
 Universe.prototype.run = function()
@@ -20,53 +34,168 @@ Universe.prototype.run = function()
 
 Universe.prototype.createNoise = function()
 {
-    var width  = this.stage.getWidth();
-    var height = this.stage.getHeight();
+    this.particles = [];
 
-    this.stage.clear();
-
-    var layer  = new Kinetic.Layer();
     var noise  = new Noise();
 
-    for (var y = 0; y < height; y += this.particleSize)
+    for (var y = 0; y < this.size.height; y++)
     {
-        for (var x = 0; x < width; x += this.particleSize)
+        this.particles[y] = [];
+
+        for (var x = 0; x < this.size.width; x++)
         {
-            var intensity = Math.abs(noise.smoothedNoise(x / this.particleSize, y / this.particleSize));
+            var mass = Math.abs(noise.smoothedNoise(x, y));
 
-            //log(intensity);
+            //log(mass);
 
-            var particle = new Particle(x, y, intensity, this.particleSize);
-            layer.add(particle.shape);
+            var particle = new Particle(x, y, mass, this.particleSize);
+            this.layer.add(particle.shape);
+
+            this.particles[y][x] = particle;
         }
     }
 
-    this.stage.add(layer);
+    this.render();
 };
 
-
-var Particle = function(x, y, intesity, size)
+Universe.prototype.render = function()
 {
-    this.intesity = intesity;
-    this.size     = size;
+    this.stage.clear();
+    this.layer.clear();
+    this.layer.draw();
+}
 
-    var color     = this.getColor();
+Universe.prototype.nextFrame = function()
+{
+    this.updateParticlesArray();
+
+    for (var y = 0; y < this.size.height; y++)
+    {
+        for (var x = 0; x < this.size.width; x++)
+        {
+            var particle = this.particles[y][x];
+
+            if (!particle) continue;
+
+            for (var ry = y-this.gravityRadius; ry <= y+this.gravityRadius; ry++)
+            {
+                for (var rx = x-this.gravityRadius; rx <= x+this.gravityRadius; rx++)
+                {
+                    if (ry < 0 || this.size.height <= ry || rx < 0 || this.size.width <= rx) continue;
+
+                    var forceVec = {
+                        x: particle.x - rx,
+                        y: particle.y - ry
+                    };
+
+                    if (forceVec.x == 0 && forceVec.y == 0) continue;
+
+                    var vecLength = Math.abs(forceVec.x) + Math.abs(forceVec.y);
+                    if (vecLength > this.gravityRadius) continue;
+
+                    var g = particle.mass * this.gravityFactor * (vecLength / Math.pow(vecLength, 2));
+
+                    var affectedParticle = this.particles[ry][rx];
+
+                    if (!affectedParticle) continue;
+
+                    affectedParticle.force.x += forceVec.x * g;
+                    affectedParticle.force.y += forceVec.y * g;
+                }
+            }
+        }
+    }
+
+    for (var y = 0; y < this.size.height; y++)
+    {
+        for (var x = 0; x < this.size.width; x++)
+        {
+            var particle = this.particles[y][x];
+
+            if (!particle) continue;
+
+            particle.render();
+        }
+    }
+
+    this.render();
+};
+
+Universe.prototype.updateParticlesArray = function()
+{
+    var particles = [];
+
+    for (var y = 0; y < this.size.height; y++)
+    {
+        particles[y] = [];
+
+        for (var x = 0; x < this.size.width; x++)
+        {
+            particles[y][x] = null;
+        }
+    }
+
+    for (var y = 0; y < this.size.height; y++)
+    {
+        for (var x = 0; x < this.size.width; x++)
+        {
+            var particle = this.particles[y][x];
+
+            if (particle && particle.x > 0 && particle.y > 0 && this.size.width > particle.x && this.size.height > particle.y)
+            {
+                if (particles[particle.y][particle.x])
+                {
+                    particles[particle.y][particle.x].mass += particle.mass;
+                }
+                else
+                {
+                    particles[particle.y][particle.x] = particle;
+                }
+            }
+        }
+    }
+
+    this.particles = particles;
+}
+
+
+var Particle = function(x, y, mass, size)
+{
+    this.x     = x;
+    this.y     = y;
+    this.mass  = mass;
+    this.size  = size;
+    this.force = {x: 0, y: 0};
 
     this.shape = new Kinetic.Rect({
         width       : this.size,
         height      : this.size,
-        strokeWidth : 0,
-        fill        : color,
-        x           : x,
-        y           : y,
-        opacity     : 1
+        strokeWidth : 0
     });
+
+    this.render();
 };
 
-Particle.prototype.getColor = function()
+Particle.prototype.render = function()
 {
-    var r = Math.floor(this.intesity * 106);
-    var g = Math.floor(this.intesity * 27);
-    var b = Math.floor(this.intesity * 224);
+    this.x = Math.round(this.x + this.force.x);
+    this.y = Math.round(this.y + this.force.y);
+
+    this.shape.setFill(this.computeColor());
+
+    var stagePos = this.computeStagePosition();
+    this.shape.setPosition(stagePos.x, stagePos.y);
+};
+
+Particle.prototype.computeStagePosition = function()
+{
+    return {x: this.x * this.size, y: this.y * this.size};
+};
+
+Particle.prototype.computeColor = function()
+{
+    var r = Math.floor(this.mass * 106);
+    var g = Math.floor(this.mass * 27);
+    var b = Math.floor(this.mass * 224);
     return 'rgb('+r+','+g+','+b+')';
-}
+};
