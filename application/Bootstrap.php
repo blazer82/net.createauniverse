b4a8ea13d33d5b12128a6a817c31a03a9e242103
Raphael Stäbler <blazer@fantasymail.de>
2012-09-09 01:15:48 +0200

    MOD: layout stuff

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,10 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    protected function _initDoctype()
+    {
+        $documentType = new Zend_View_Helper_Doctype();
+        $documentType->doctype(Zend_View_Helper_Doctype::HTML5);
+    }
 }
 
